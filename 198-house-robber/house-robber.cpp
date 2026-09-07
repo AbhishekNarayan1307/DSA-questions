dbd3143b18f91f92b8e492a2961059856f764fa3
AbhishekNarayan1307 <abhishek.an36@gmail.com>
2026-09-07 19:14:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (46.72%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    // int solve(int i, int n, unordered_map<int, int> &dp, vector<int>& nums ){
+    //     if(i>=n) return 0;
+    //     if(dp.find(i) != dp.end()) return dp[i];
+    //     int steal = nums[i] + solve(i+2, n, dp, nums );
+    //     int skip = solve(i+1, n, dp, nums);
+    //     dp[i] = max(steal, skip);
+    //     return dp[i];
+    // }
+    int rob(vector<int>& nums) {
+        // unordered_map<int, int> dp;
+        // return solve(0, nums.size(), dp, nums);
+        int n = nums.size();
+        vector<int>dp(n+2, 0);
+        dp[n] = 0;
+        for(int i = n -1; i>=0; i--){
+            dp[i] = max(nums[i] + dp[i+2], dp[i+1]);
+        }
+        return dp[0];
+    }
+};
